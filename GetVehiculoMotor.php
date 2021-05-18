37243f16e56949fa648a97122d169e2fb0b6dc17
Victor H. Diaz <victor.diaz@optic.gob.do>
2021-05-18 11:44:41 -0400

    creating the methods for the getVehiculoMotor Class

---
+++ GetVehiculoMotor.php
@@ -0,0 +1,30 @@
+<?php
+
+class GetVehiculoMotor{
+
+
+    protected function getVigencia($object){
+        echo 'getVigencia';
+    }
+
+    protected function getObjetoAsegurado($object){
+        echo 'getObjetoAsegurado';
+    }
+
+    protected function getCobertura($object){
+        echo 'getCobertura';
+    }
+
+    protected function updateEachObjectToSingleLevel($object){
+        echo 'updateEachObjectToSingleLevel';
+    }
+
+    protected function seFormater($object){
+        $this->updateEachObjectToSingleLevel($object);
+        $this->getCobertura($object);
+        $this->getObjetoAsegurado($object);
+        $this->getVigencia($object);
+    }
+}
+
+?>
